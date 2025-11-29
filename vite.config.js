b8f4c48f83305f3e31b3700b2af0a80a80c3cc59
Takeru0412-debug <takerutsukareta0412@gmail.com>
2025-11-29 20:24:13 +0900

    Fix Vite base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-tetris-board/', // ← リポジトリ名をそのまま書く
+  base: './',   // ← これだけでOK（Netlify推奨）
 })
